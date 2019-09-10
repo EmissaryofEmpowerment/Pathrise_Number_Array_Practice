e6a623cf1942ec5bb4ec7bbb0fd75c525a2082da
ryscottarmstrong <ryscottarmstrong@gmail.com>
2019-09-09 19:12:27 -0600

    Deposited the completed program into the recently cloned project.

---
+++ Number_Array_Practice.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Pathrise_NumberArray_Practice
+{
+    partial class Number_Array_Practice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(503, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Given an array of numbers, describe how you would find any duplicates. If you thi" +
+    "nk of multiple approaches, describe the tradeoffs. It\'s encouraged to add compil" +
+    "able code for one solution.";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(248, 478);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(-2, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(571, 433);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Number_Array_Practice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 505);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label1);
+            this.Name = "Number_Array_Practice";
+            this.Text = "Number_Array_Practice";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Number_Array_Practice.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pathrise_NumberArray_Practice
+{
+    public partial class Number_Array_Practice : Form
+    {
+        public Number_Array_Practice()
+        {
+            InitializeComponent();
+            listBox1.Items.Add("hello");
+            listBox1.Items.Add("second line");
+ //The method ShowResults() calls the method 
+  //GenerateNumberArray() which in turn calls SearchForDuplicates()
+          ShowResults();
+        }
+
+        public void ShowResults()
+        {
+            Queue<List<int>> Duplicates = SearchForDuplicates();
+            foreach (var item in Duplicates)
+            {
+                listBox1.Items.Add($"Positions {item[0] + 1}" +
+                    $" and {item[1] + 1} share a duplicate " +
+                    $"value of {item[2]}");
+            }
+        }
+
+        public Queue<List<int>> SearchForDuplicates()
+        {
+            int[] NumberArray = GenerateNumberArray();
+            Queue<List<int>> Duplicates = new Queue<List<int>>();
+            for (int a = 0; a < NumberArray.Length; a++)
+            {
+                for (int b = a + 1; b < NumberArray.Length; b++)
+                {
+                    if (NumberArray[a] == NumberArray[b])
+                    {
+                        List<int> LocationAndValue = new List<int>(3);
+                        LocationAndValue.Add(a);
+                        LocationAndValue.Add(b);
+                        LocationAndValue.Add(NumberArray[a]);
+                        Duplicates.Enqueue(LocationAndValue);
+                    }
+                }
+            }
+            return Duplicates;
+        }
+
+        public int[] GenerateNumberArray()
+        {
+            int[] NumberArray = new int[10];
+            Random rand = new Random();
+            for (int i = 0; i < NumberArray.Length; i++)
+            {
+                NumberArray[i] = rand.Next(9);
+            }
+            return NumberArray;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
